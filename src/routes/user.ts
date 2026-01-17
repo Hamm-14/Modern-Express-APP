8bac4e7fc5416edfc3faf8c5054037ce8cd83950
Hammad Khan <hammadyasub14@gmail.com>
2026-01-17 20:50:01 +0530

    introduce validate schema as a middleware and put logic inside controller

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,7 @@
 import express, { Response } from "express";
-import { User } from "../entity/User";
-import { AppDataSource } from "../datasource";
+import { validateSchema } from "../middlewares/validate";
+import { CreateUserSchema } from "../schemas/user";
+import { createUserController } from "../controllers/user";
 
 const userRouter = express.Router();
 
@@ -8,27 +9,17 @@ userRouter.get("/", (_, res: Response) => {
   res.send("User route called...");
 });
 
-userRouter.post("/", async (req, res: Response) => {
-  try {
-    const { email, firstName, lastName, age } = req.body;
-    const user = new User(email, firstName, lastName, age);
-
-    const userRepository = AppDataSource.getRepository(User);
-    const isUserExist = await userRepository.findOneBy({ email });
-
-    if (isUserExist) {
-      return res
-        .status(400)
-        .json({ message: "User with this email already exists" });
+userRouter.post(
+  "/",
+  validateSchema(CreateUserSchema),
+  async (req, res: Response) => {
+    try {
+      return createUserController(req, res);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ message: "Internal Server Error" });
     }
-
-    await userRepository.save(user);
-
-    res.status(201).json(user);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
+  },
+);
 
 export default userRouter;
