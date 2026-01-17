c471c89d35bb952ee2e08d6e0a73ab44bd3ba3fd
Hammad Khan <hammadyasub14@gmail.com>
2026-01-17 15:08:24 +0530

    Fix: .gitignore now properly ignoring files

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
 
-    @PrimaryGeneratedColumn()
-    id: number
+  @Column({ type: "varchar" })
+  firstName: string;
 
-    @Column()
-    firstName: string
+  @Column({ type: "varchar" })
+  lastName: string;
 
-    @Column()
-    lastName: string
+  @Column({ type: "int" })
+  age: number;
 
-    @Column()
-    age: number
-
+  constructor(firstName: string, lastName: string, age: number) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import "reflect-metadata";
 import "dotenv/config";
 import express, { Application, Response } from "express";
+import { AppDataSource } from "./data-source";
 
 const app: Application = express();
 const PORT = process.env.PORT || 4000;
@@ -7,6 +9,13 @@ const PORT = process.env.PORT || 4000;
 app.get("/", (_, res: Response) => {
   res.send("Home path called...");
 });
+
+try {
+  await AppDataSource.initialize();
+  console.log("Database connected successfully");
+} catch (err) {
+  console.log(`Error connecting database ${err}`);
+}
 
 app.listen(PORT, () => {
   console.log(`Server is up an running on port: ${PORT}`);
